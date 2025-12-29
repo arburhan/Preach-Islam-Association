7ed9d48f48df7b9880d232f1094ff5636710ba45
arburhan <burhan128454@outlook.com>
2025-12-29 23:10:32 +0600

    statistics card added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Hero from "@/components/home/hero";
+import { StatsSection } from "@/components/home/statsCard";
 
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <Hero />
-      
+      <StatsSection />
     </section>
   );
 }
