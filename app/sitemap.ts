1f1509ff1ca49c0241468b738a9a0906de5b8dda
arburhan <burhan128454@outlook.com>
2026-03-09 11:26:24 +0600

    try to fix seo issue

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://preachislam.org';
+    const baseUrl = 'https://www.preachislam.org';
 
     return [
         {
